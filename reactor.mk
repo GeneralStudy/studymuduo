fce330b8536b9bdab7f1097385b4312320d8dca3
Hill1942 <ykdacd123@gmail.com>
2018-04-11 21:18:41 +0800

    make rule

--- reactor.mk
+++ reactor.mk
@@ -3,7 +3,7 @@ LDFLAGS = -lpthread
 BASE_SRC = ../logging/Logging.cc ../logging/LogStream.cc ../thread/Thread.cc ../datetime/Timestamp.cc
 
 $(BINARIES):
-	g++ $(CXXFLAGS) -o $@ $(LIB_SRC) $(BASE_SRC) $(filter %.cc,$^) $(LDFLAGS)
+	g++ $(CXXFLAGS) -o bin/$@ $(LIB_SRC) $(BASE_SRC) $(filter %.cc,$^) $(LDFLAGS)
 
 clean:
-	rm -f $(BINARIES) core
+	rm -f bin/* core
